6f7ea9382b2e266c5349d943d816f287071e38fd
sarthak <sarthakgaud04@gmail.com>
2024-07-04 22:25:01 +0530

    unused imports uuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
 'use client'
 
-import Image from "next/image";
 import {BGbeams} from "@/components/BGbeams";
 import React from "react";
 import {InfiniteCards} from "@/components/InfiniteCards";
-import ProblemListDemo from "@/components/questions/Datatable";
 import {WobbleCards} from "@/components/WobbleCards";
-import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
 export default function Home() {
